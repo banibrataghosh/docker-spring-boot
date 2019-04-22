ab7578ca1b787036021c844fc43665e4018da26e
Banibrata <banibrata.ghosh.07@gmail.com>
2019-04-22 20:54:41 +0530

    First commit

---
+++ src/main/java/com/sample/docker/docker/DockerController.java
@@ -0,0 +1,15 @@
+package com.sample.docker.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class DockerController {
+
+	@GetMapping
+	public String hello() {
+		return "Hello Banibrata, Welcome to docker world";
+	}
+}
